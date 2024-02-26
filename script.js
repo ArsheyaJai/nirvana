afb13961b7985ea62272ad9d001da3a39b685a4c
ArsheyaJai <137911489+ArsheyaJai@users.noreply.github.com>
2024-02-26 23:15:25 +0000

    updates

---
+++ script.js
@@ -0,0 +1,5 @@
+alert("Taylor Swift's New Album The Tortured Poet's Department Out April 19th!");
+
+let year = 2024;
+let artist = "Taylor Swift";
+console.log(year + "is the year of album drops!");
