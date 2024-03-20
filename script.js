4f7167e84bae8d22b15fbbe338456dd4fe15439f
ArsheyaJai <137911489+ArsheyaJai@users.noreply.github.com>
2024-03-20 20:26:36 +0000

    updated js

--- script.js
+++ script.js
@@ -1,4 +1,22 @@
 let year = 2024;
 let artist = "Taylor Swift";
-alert(artist + "'s New Album The Tortured Poet's Department Out April 19th!");
+let date = 10+9;
+alert(artist + "'s New Album The Tortured Poet's Department Out April " + date + "th!");
 console.log(year + " is the year of album drops!");
+
+
+//countdown to album release
+let text;
+//today's date
+let today = new Date();
+let release = new Date();
+//date of album release
+release.setFullYear(2024, 3, 19);
+
+if (today = release || release < today) {
+  //TTPD = The Tortured Poet's Department
+  text = "Check out TTPD!";
+} else {
+  text = "TTPD did not drop yet :(";
+}
+console.log(text);
